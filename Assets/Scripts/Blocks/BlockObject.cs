d8f29b08cdfa29f9fd05b36af8822713d9f69c78
Максим Юдин <myudin@k-solutions.ru>
2024-06-27 12:00:22 +0300

    1

--- Assets/Scripts/Blocks/BlockObject.cs
+++ Assets/Scripts/Blocks/BlockObject.cs
@@ -9,5 +9,4 @@ public class BlockObject : ScriptableObject
     public float Bounciness = 0;
     public float Weight = 1;
     public float Scale = 1;
-    public BlockForm Form = BlockForm.Square;
 }
